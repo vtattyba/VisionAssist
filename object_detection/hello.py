d5f5d804131023e41070b4cfc9a2af7c5b9c677e
Vladimir <vtattyba@buffalo.edu>
2019-11-05 00:58:04 -0500

    Added Obj_Detection File With Model

---
+++ object_detection/hello.py
@@ -0,0 +1,13 @@
+from distance_sensor import *
+from Object_detection_picamera import * 
+
+
+triggerOne = True 
+triggerTwo = True 
+
+                  
+if triggerOne is True and triggerTwo is True : 
+	print ("Calling Script Works")
+
+#if triggerTwo is True : 
+#	print "test" 
